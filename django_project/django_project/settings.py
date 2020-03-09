001d758a7869899f41376bf25506fdb282f129bb
gaohj <gaohj@163.com>
2020-03-09 14:44:57 +0800

    online1

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1bru+lof-6y03yd=58rrtpl_jn6j@)wlnq+i(dg8ah&tl_3*)%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
